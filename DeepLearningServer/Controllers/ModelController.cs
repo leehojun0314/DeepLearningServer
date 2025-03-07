9e5467fe48ed8a017a13ca96aa7bd21b6cc2c211
leehojun0314 <dlghwns0314@naver.com>
2025-03-07 19:47:16 +0900

    model upgrade fin

--- DeepLearningServer/Controllers/ModelController.cs
+++ DeepLearningServer/Controllers/ModelController.cs
@@ -1,5 +1,6 @@
 ﻿using Euresys.Open_eVision.EasyDeepLearning;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -28,7 +29,7 @@ namespace DeepLearningServer.Controllers
             {
                 string oldModelsPath = "D:\\ModelUpgradeProject\\old";  // 기존 모델 폴더
                 string newModelsPath = "D:\\ModelUpgradeProject\\new"; // 새로운 모델 저장 폴더
-
+                string projectDir = "D:\\ModelUpgradeProject\\project";
                 // 새 폴더가 없으면 생성
                 if (!Directory.Exists(newModelsPath))
                 {
@@ -37,39 +38,68 @@ namespace DeepLearningServer.Controllers
 
                 // 기존 모델 파일들 가져오기
                 string[] modelFiles = Directory.GetFiles(oldModelsPath, "*.edltool");
+                if (Directory.Exists(projectDir))
+                {
+                    Directory.Delete(projectDir, true);
+                }
+                Console.WriteLine("Creating project...");
+                // 새 프로젝트 생성
+                EDeepLearningProject project;
 
+               
+                    project = new EDeepLearningProject();
+                    project.Type = EDeepLearningToolType.EasyClassify;
+                    project.Name = "modelUpgrade";
+                    project.ProjectDirectory = projectDir;
+                    project.SaveProject();
+                    Console.WriteLine("Saved project.");
+                    //EDeepLearningProject project = new EDeepLearningProject();
+                    // 모델 파일 업그레이드
+                    //EDeepLearningTool tool = EDeepLearningTool.Create(modelFile);
+                    //project.ImportTool(fileName, tool);
+                //project.Name = "modelUpgrade.edlproj";
+                
+                int toolIndex = 0;
                 foreach (string modelFile in modelFiles)
                 {
                     string fileName = Path.GetFileName(modelFile);
                     try
                     {
                         Console.WriteLine("Upgrading tool: " + modelFile);
-                        // 새 프로젝트 생성
-                        EDeepLearningProject project = new EDeepLearningProject();
-                        string projectPath = Path.Combine(newModelsPath, "project.edlproj");
-                        project.Save(projectPath);
 
-                        // 모델 파일 업그레이드
-                        project.ImportTool(modelFile, Path.Combine(newModelsPath, fileName));
-                        
-                        if (project.HasFileStructureUpdates())
-                        {
-                            Console.WriteLine("The project has file structure updates");
-                            project.UpdateProjectFileStructure();
-                            project.SaveProject();
-                            string newModelPath = Path.Combine(newModelsPath, Path.GetFileName(modelFile));
-                            EDeepLearningTool tool = project.GetToolCopy(0);
-                            tool.SaveTrainingModel(newModelPath);
-                        }
-                        else
-                        {
-                            Console.WriteLine("The project doesn't have file structure updates");
-                        }
-                       
+                        //project.Save(projectDir);
+                        Console.WriteLine("importing tool...");
+                        project.ImportTool($"Tool{toolIndex}", modelFile);
+                        Console.WriteLine("saving project...");
+                        Console.WriteLine("Save 2...");
+                        //project.Save(projectDir);
+                        //if (project.HasFileStructureUpdates())
+                        //{
+                        //Console.WriteLine("The project has file structure updates");
+                        Console.WriteLine("UpdateProjectFileStructure");
+                        project.UpdateProjectFileStructure();
+                        //project.SaveProject();
+                        string newModelPath = Path.Combine(newModelsPath, Path.GetFileName(modelFile));
+                        Console.WriteLine("New model path: " + newModelPath);
+                        EDeepLearningTool newTool = project.GetToolCopy(toolIndex);
+                        toolIndex++;
+                        Console.WriteLine("Saving model...");
+                        newTool.SaveTrainingModel(newModelPath);
+                        Console.WriteLine("Mode saved");
+                        //project.Dispose();
+                        //newTool.Dispose();
+                        //Directory.Delete(projectDir);
+                        //}
+                        //else
+                        //{
+                        //Console.WriteLine("The project doesn't have file structure updates");
+                        //}
+
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine($"모델 업그레이드 실패: {modelFile}, 오류: {ex.Message}");
+                        throw new Exception(ex.Message);
                     }
                 }
 
